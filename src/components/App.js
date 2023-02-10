2f6bedabf3fb544bf605da05a72f898d0b62df91
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-02-10 11:11:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
+import appStyles from '../styles/appStyles.module.css';
 const App = () => {
   const heading = {
-
 
   }
   return (
     <div>
-      <h1 className={styles.appStyles.module.success}>Success</h1>
+      <h1 className={appStyles.success}>Success</h1>
       <h1 style={{ fontSize: '50px', color: 'red' }}>Error</h1>
     </div>
   )
